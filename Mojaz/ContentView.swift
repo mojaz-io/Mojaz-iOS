b8be09d00e7f156fee3ec49d2029eeb7fbc5919c
Moski Doski <abushaikh@gmail.com>
2019-12-16 19:50:10 +0400

    Initial Commit

---
+++ Mojaz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mojaz
+//
+//  Created by Moski Doski on 12/16/19.
+//  Copyright © 2019 Moski Doski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
